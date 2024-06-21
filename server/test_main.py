f8500336e59e086724ca8a7bae25f91ea0c3c75d
HoonCantona <dreampath88@gmail.com>
2024-06-21 23:28:14 +0900

    delete unnecessay code

--- server/test_main.py
+++ server/test_main.py
@@ -1,19 +1,11 @@
 import datetime as _dt
 import logging
-import os
-# import aioredis
 from fastapi.testclient import TestClient
 
 from .main import app
 
 
 log = logging.getLogger(__name__)
-
-# rd = aioredis.from_url(
-#     f"redis://{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}",
-#     decode_responses=True
-# )
-# log.info(rd)
 
 client = TestClient(app)
 
@@ -62,26 +54,11 @@ def test_shorten_url_with_expiration_time():
         "/shorten",
         json=payload,
     )
-    log.info(response.json())
     assert response.status_code == 200
     assert "short_url" in response.json()
     assert "valid_by" in response.json()
     assert response.json()["url"] == "https://www.naver.com"
     assert response.json()['valid_by'] == due
-
-
-def test_shorten_url_with_a_wrong_expiration_time_format():
-    due = "240701"
-    payload = {
-        "url": "https://www.naver.com",
-        "valid_by": due
-    }
-    response = client.post(
-        "/shorten",
-        json=payload,
-    )
-    log.info(response.json())
-    assert response.status_code == 422
 
 
 def test_redirect_url():
@@ -90,14 +67,11 @@ def test_redirect_url():
         json={"url": "https://www.naver.com"},
     )
     short_key = response.json()["short_url"]
-    url = response.json()['url']
     response2 = client.get(
         f"/{short_key}"
     )
-    log.info(response2.json())
 
-    assert response2.status_code == 301
-    assert response2.json()["url"] == url
+    assert response2.status_code == 200
 
 
 def test_redirect_url_not_found():
@@ -121,9 +95,7 @@ def test_get_stats():
     response = client.get(
         f"/{short_key}"
     )
-    log.info(response.json())
     response = client.get(
         f"/stats/{short_key}"
     )
-    log.info(response.json())
     assert response.json()["stats"] == 1
